72a0d845d0612c34c9cdfd318aa0449ba8f9cfab
matosluizdev <luizmatosdev@outlook.com>
2024-05-27 16:21:10 -0300

    fc

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cardápio</title>
+    <!-- Tailwind CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <style>
+        body, html {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+</head>
+<body class="bg-gray-900 flex justify-center items-center">
+    <div class="w-full h-full flex flex-col items-center mt-5">
+        <!-- Ícones e textos -->
+        <div class="w-full flex flex-col items-center mt-5 px-4">
+        <div class="mb-4 flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-8 text-gray-700 mt-5">
+            <div class="flex items-center space-x-2">
+                <i class="fas fa-phone-alt text-white"></i>
+                <span class="text-white text-sm sm:text-lg">(18) 99812-4945</span>
+            </div>
+            <div class="flex items-center space-x-2">
+                <i class="fas fa-map-marker-alt text-white"></i>
+                <span class="text-white text-sm sm:text-lg">Av. Baguaçu, 311 - Jardim Iguatemi, Guararapes - SP</span>
+            </div>
+        </div>
+        </div>
+        <!-- Container do iframe -->
+        <div class="w-full h-full flex justify-center items-center">
+            <iframe allow="fullscreen" class="w-4/5 h-5/6 rounded-lg shadow-lg" 
+                src="https://www.canva.com/design/DAGEXu5-8kc/HOoOHGPhxO0YYOIvJK7vRw/view?embed">
+            </iframe>
+        </div>
+    </div>
+</body>
+</html>
